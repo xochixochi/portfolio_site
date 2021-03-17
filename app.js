13ad7fd19ab7ea9ad5a05237d27f82d7a9300620
jkinsfat <jasonrkinsfather@gmail.com>
2021-03-17 16:01:17 -0700

    Add virtual mount to serve static images linked in css.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const indexRouter = require('./routes/index');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use('/image', express.static(path.join(__dirname, './public/images/')));
 app.use(express.static(path.join(__dirname, './public/')));
 
 app.use('/', indexRouter);
